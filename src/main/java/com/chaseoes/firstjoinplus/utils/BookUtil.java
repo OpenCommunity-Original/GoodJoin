094774b1d664987493709bbffc8821d2a8b3374b
Envel <52075465+Envel-Nikita-Gutsenkov@users.noreply.github.com>
2023-05-14 00:14:40 +0300

    cleanup

--- src/main/java/com/chaseoes/firstjoinplus/utils/BookUtil.java
+++ src/main/java/com/chaseoes/firstjoinplus/utils/BookUtil.java
@@ -8,7 +8,6 @@ import net.kyori.adventure.inventory.Book;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.ComponentBuilder;
 import net.kyori.adventure.text.event.ClickEvent;
-import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextColor;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.entity.Player;
@@ -19,9 +18,6 @@ import java.util.List;
 import java.util.regex.Pattern;
 
 public class BookUtil {
-
-    // Regular expression to match URLs in the message
-    private static final Pattern URL_PATTERN = Pattern.compile("(https?://\\S+)");
 
     public static void openBook(Player player) {
         String message = LocaleAPI.getMessage(player, "rules");
@@ -69,19 +65,5 @@ public class BookUtil {
                 .build();
 
         player.openBook(book);
-    }
-
-
-    // Format a page of text with color and style
-    private static Component formatPage(String text) {
-        return LegacyComponentSerializer.legacyAmpersand().deserialize(text);
-    }
-
-    // Format a clickable link
-    private static String formatLink(String url) {
-        Component link = Component.text(url).color(NamedTextColor.BLUE)
-                .clickEvent(ClickEvent.openUrl(url))
-                .hoverEvent(Component.text("Click to open link").color(NamedTextColor.GRAY));
-        return link.toString();
     }
 }
